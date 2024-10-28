3700778b7f62e152b44ee1ea41980fa1a2414bd2
Kabir <kabirsync1@gmail.com>
2024-10-28 23:21:09 +0000

    refactor: responsie border on controls

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
           <div className="flex-grow">
             <Simulation />
           </div>
-          <div className=" min-h-36 md:w-72 border-l border-zinc-800 p-4">
+          <div className="min-h-36 md:w-72 border-t md:border-t-0 md:border-l border-zinc-800 p-4">
             <h1>Controls</h1>
           </div>
         </div>
