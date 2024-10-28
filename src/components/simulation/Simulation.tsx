7b7fffc6811e111d5e3419364d19f71c6aab58d4
Kabir <kabirsync1@gmail.com>
2024-10-28 21:30:11 +0000

    refactor: move simulation to own folder

---
+++ src/components/simulation/Simulation.tsx
@@ -0,0 +1,72 @@
+import { backgroundColor, sandColor, squareTexture } from "@/lib/colors";
+import { Grid } from "@/simulations/Grid";
+import { ParticleContainer, Sprite, useTick } from "@pixi/react";
+import { Sprite as SpriteType } from "pixi.js";
+import { MutableRefObject, useRef } from "react";
+
+type SimulationProps = {
+  columns: number;
+  rows: number;
+  gridRef: MutableRefObject<Grid | undefined>;
+  grainWidth: number;
+};
+
+const Simulation = ({
+  columns,
+  rows,
+  gridRef,
+  grainWidth,
+}: SimulationProps) => {
+  const spriteRefs = useRef<(SpriteType | null)[]>([]);
+
+  useTick(() => {
+    if (gridRef.current) {
+      gridRef.current.grid.forEach((item, index) => {
+        const sprite = spriteRefs.current[index];
+        if (sprite) {
+          // Update sprite properties without re-rendering React
+          sprite.tint = item === 0 ? backgroundColor : sandColor;
+          sprite.x = (index % columns) * grainWidth;
+          sprite.y = (rows - Math.floor(index / columns)) * grainWidth;
+        }
+      });
+    }
+  });
+
+  if (!gridRef.current) {
+    return null;
+  }
+
+  return (
+    <ParticleContainer
+      maxSize={columns * rows}
+      properties={{
+        scale: true,
+        position: true,
+        alpha: true,
+        tint: true,
+      }}
+    >
+      {gridRef.current.grid.map((_, index) => {
+        const gridItemColumn = index % columns;
+        const gridItemRow = Math.floor(index / columns);
+        const x = gridItemColumn * grainWidth;
+        const y = (rows - gridItemRow) * grainWidth;
+
+        return (
+          <Sprite
+            key={index}
+            texture={squareTexture}
+            x={x}
+            y={y}
+            width={grainWidth - 2}
+            height={grainWidth - 2}
+            ref={(sprite) => (spriteRefs.current[index] = sprite)} // Store sprite reference
+          />
+        );
+      })}
+    </ParticleContainer>
+  );
+};
+
+export default Simulation;
