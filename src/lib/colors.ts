bc6622278c86855b0fc626282d7237ae34e44215
Kabir <kabirsync1@gmail.com>
2024-10-28 22:06:52 +0000

    feature: change pixi stage background color on theme change

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -4,4 +4,5 @@ export const squareTexture = PIXI.Texture.WHITE; // Create a single neutral base
 export const grainColor = new PIXI.Color("#d4d4d8"); // Light
 export const sandColor = new PIXI.Color("#dcb159");
 export const backgroundColor = new PIXI.Color("#09090b");
-export const backgroundColorNumerical = 0x09090b; // Dark - for Stagec component as it does not accept Pixi.Color needs numerical color
+export const backgroundColorDarkNumerical = 0x09090b; // Dark - for Stagec component as it does not accept Pixi.Color needs numerical color
+export const backgroundColorLightNumerical = 0xd4d4d8; // Light - for Stagec component as it does not accept Pixi.Color needs numerical color
