8f89670517fa3c613245096687b5af8873fb79c4
Kabir <kabirsync1@gmail.com>
2024-10-28 21:47:54 +0000

    feature: change bg on theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from "@/components/theme/ThemeProvider";
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="h-dvh flex flex-col">
+      <div className="h-dvh flex flex-col bg-zinc-300 dark:bg-zinc-950">
         <Navbar />
         <div className="h-full flex flex-col md:flex-row">
-          <div className="bg-zinc-700 flex-grow">
+          <div className="flex-grow">
             <Simulation />
           </div>
-          <div className="bg-zinc-600 min-h-36 md:w-72">
+          <div className=" min-h-36 md:w-72 border-l border-zinc-800 p-4">
             <h1>Controls</h1>
           </div>
         </div>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -12,11 +12,11 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
   console.log({ theme });
   return (
-    <nav className="h-12 bg-zinc-800 flex justify-between items-center px-4">
+    <nav className="h-12 flex justify-between items-center px-4 border-b border-zinc-800">
       <p>Navbar</p>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
+          <Button variant="ghost" size="icon">
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle theme</span>
