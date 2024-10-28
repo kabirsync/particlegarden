af6e899cdc0aafe9976d477ff07e68946e7d0a75
Kabir <kabirsync1@gmail.com>
2024-10-28 21:30:42 +0000

    feature: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import Simulation from "@/components/SimulationContainer";
+import { ThemeProvider } from "@/components/theme/ThemeProvider";
 
 function App() {
   return (
-    <div className="h-dvh flex flex-col">
-      <nav className="h-12 bg-zinc-800">
-        <p>Navbar</p>
-      </nav>
-      <div className="h-full flex flex-col md:flex-row">
-        <div className="bg-zinc-700 flex-grow">
-          <Simulation />
-        </div>
-        <div className="bg-zinc-600 min-h-36 md:w-72">
-          <h1>Controls</h1>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="h-dvh flex flex-col">
+        <nav className="h-12 bg-zinc-800">
+          <p>Navbar</p>
+        </nav>
+        <div className="h-full flex flex-col md:flex-row">
+          <div className="bg-zinc-700 flex-grow">
+            <Simulation />
+          </div>
+          <div className="bg-zinc-600 min-h-36 md:w-72">
+            <h1>Controls</h1>
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="h-12 bg-zinc-800">
+      <p>Navbar</p>
+    </nav>
+  );
+};
+
+export default Navbar;
