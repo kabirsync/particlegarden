4e45c3e63455f9050b46b46b542feecaae64f51a
Kabir <kabirsync1@gmail.com>
2024-10-29 04:47:02 +0000

    refactor: change light theme color

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -3,7 +3,7 @@ import * as PIXI from "pixi.js";
 export const squareTexture = PIXI.Texture.WHITE; // Create a single neutral base texture for the squares
 export const sandColor = new PIXI.Color("#dcb159");
 export const backgroundColorDark = new PIXI.Color("#09090b");
-export const backgroundColorLight = new PIXI.Color("#d4d4d8");
+export const backgroundColorLight = new PIXI.Color("#f4f4f5");
 
 export const backgroundColorDarkNumerical = 0x09090b; // Dark - for Stagec component as it does not accept Pixi.Color needs numerical color
-export const backgroundColorLightNumerical = 0xd4d4d8; // Light - for Stagec component as it does not accept Pixi.Color needs numerical color
+export const backgroundColorLightNumerical = 0xf4f4f5; // Light - for Stagec component as it does not accept Pixi.Color needs numerical color
