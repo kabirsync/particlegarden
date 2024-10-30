5099480827bc8439c0d9fbfbeff49657335fdc10
Kabir <kabirsync1@gmail.com>
2024-10-30 01:32:03 +0000

    feature: add material type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,9 @@
+import type Empty from "@/simulations/Empty";
+import type Sand from "@/simulations/Sand";
+
 export type Dimension = {
   width: number;
   height: number;
 };
+
+export type Materials = Empty | Sand;
