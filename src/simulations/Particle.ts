d7ad9f224dfcdd690d1311121f8282894c75dc6b
Kabir <kabirsync1@gmail.com>
2024-10-30 00:11:33 +0000

    feature: add Empty class

--- src/simulations/Particle.ts
+++ src/simulations/Particle.ts
@@ -2,15 +2,18 @@ import { Color } from "pixi.js";
 
 type ParticleOptions = {
   color: Color;
+  isEmpty: boolean;
 };
 
 class Particle {
   index: number;
   color: Color;
+  isEmpty: boolean;
 
-  constructor(index: number, { color }: ParticleOptions) {
+  constructor(index: number, { color, isEmpty = false }: ParticleOptions) {
     this.index = index;
     this.color = color;
+    this.isEmpty = isEmpty;
   }
 }
 
