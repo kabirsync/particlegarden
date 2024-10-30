e8f2405d140cd643fd5f78f5f6c4ac9948f2042d
Kabir <kabirsync1@gmail.com>
2024-10-30 02:59:28 +0000

    feature: specify locations required to add new materials

--- src/components/simulation/materials/Material.ts
+++ src/components/simulation/materials/Material.ts
@@ -2,7 +2,12 @@ import Sand from "./Sand";
 import Empty from "./Empty";
 import { Color } from "pixi.js";
 
+// New items must be added here and at [3] points
+
+// [1] New items must be added here
 type MaterialClasses = Sand | Empty;
+
+// [2] New items must be added here
 export type MaterialOptions = "Sand" | "Empty";
 
 type MaterialProps = { color?: Color };
@@ -13,6 +18,7 @@ export const MaterialMapping: Record<
     new (index: number, { color }: MaterialProps): MaterialClasses;
   }
 > = {
+  // [3] New items must be added here
   Sand,
   Empty,
 };
