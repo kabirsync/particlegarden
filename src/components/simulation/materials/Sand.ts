1b8aacb1fed58ff0a7a951ca219763c4432b0f84
Kabir <kabirsync1@gmail.com>
2024-10-30 07:01:26 +0000

    feature: add varyColor function

--- src/components/simulation/materials/Sand.ts
+++ src/components/simulation/materials/Sand.ts
@@ -1,4 +1,4 @@
-import { sandColor } from "@/lib/colors";
+import { sandColor, varyColor } from "@/lib/colors";
 import Particle from "@/components/simulation/materials/Particle";
 import { Color } from "pixi.js";
 
@@ -8,7 +8,7 @@ type SandProps = {
 
 class Sand extends Particle {
   constructor(index: number, { color = sandColor }: SandProps) {
-    super(index, { color });
+    super(index, { color: varyColor(color) });
   }
 }
 
