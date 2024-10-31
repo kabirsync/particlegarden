db199ecd02b901b21b042028a74e4885e56a0e3b
Kabir <kabirsync1@gmail.com>
2024-10-31 04:52:44 +0000

    feature: add Behaviour class

---
+++ src/components/simulation/behaviours/Behaviour.ts
@@ -0,0 +1,5 @@
+export class Behaviour {
+  update() {
+    console.log("behvaiour");
+  }
+}
--- src/components/simulation/materials/Particle.ts
+++ src/components/simulation/materials/Particle.ts
@@ -1,19 +1,26 @@
+import { Behaviour } from "@/components/simulation/behaviours/Behaviour";
 import { Color } from "pixi.js";
 
 type ParticleOptions = {
   color: Color;
   isEmpty?: boolean;
+  behaviours?: Behaviour[];
 };
 
 class Particle {
   index: number;
   color: Color;
   isEmpty: boolean;
+  behaviours: Behaviour[];
 
-  constructor(index: number, { color, isEmpty = false }: ParticleOptions) {
+  constructor(
+    index: number,
+    { color, isEmpty = false, behaviours = [] }: ParticleOptions
+  ) {
     this.index = index;
     this.color = color;
     this.isEmpty = isEmpty;
+    this.behaviours = behaviours;
   }
 }
 
