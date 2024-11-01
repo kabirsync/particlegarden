cbb3d06ebba262dd6b4183e17ecb76ea949221fc
Kabir <kabirsync1@gmail.com>
2024-11-01 07:03:58 +0000

    refactor: remove unused code

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,11 +1,7 @@
 import { Color } from "three";
-// export const squareTexture = PIXI.Texture.WHITE; // Create a single neutral base texture for the squares
 export const sandColor = new Color("#dcb159");
 export const backgroundColorDark = new Color("#09090b");
 export const backgroundColorLight = new Color("#f4f4f5");
-
-// export const backgroundColorDarkNumerical = 0x09090b; // Dark - for Stagec component as it does not accept Pixi.Color needs numerical color
-// export const backgroundColorLightNumerical = 0xf4f4f5; // Light - for Stagec component as it does not accept Pixi.Color needs numerical color
 
 export const transparentColor = new Color("#09090b");
 
