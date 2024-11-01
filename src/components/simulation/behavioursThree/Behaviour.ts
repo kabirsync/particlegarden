20cb6759fc1f30b1b99cecc2c74dc23e1a913ec0
Kabir <kabirsync1@gmail.com>
2024-11-01 05:28:13 +0000

    refactor: remove unused files

--- src/components/simulation/behavioursThree/Behaviour.ts
+++
@@ -1,9 +0,0 @@
-import { Grid } from "@/components/simulation/GridThree";
-import Particle, {
-  Params,
-} from "@/components/simulation/materialsThree/Particle";
-
-export class Behaviour {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  update(_particle: Particle, _grid: Grid, _params: Params) {}
-}
