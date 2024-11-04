1256de3c5f034efd90d968e5ffd129fd7ebb7445
Kabir <kabirsync1@gmail.com>
2024-11-04 13:41:25 +0000

    feature: add behaviour lookup to particle

--- src/components/simulation/materials/Particle.ts
+++ src/components/simulation/materials/Particle.ts
@@ -14,12 +14,18 @@ export type Params = {
 
 export type StateOfMatter = "solid" | "liquid" | "gas" | "empty";
 
+export type BehaviorConstructor<T extends Behaviour> = new (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ...args: any[]
+) => T;
+
 class Particle {
   index: number;
   color: Color;
   stateOfMatter: StateOfMatter;
   behaviours: Behaviour[];
   modified: boolean;
+  behaviorsLookup: Map<BehaviorConstructor<Behaviour>, Behaviour>;
 
   constructor(
     index: number,
@@ -30,12 +36,24 @@ class Particle {
     this.behaviours = behaviours;
     this.modified = false;
     this.stateOfMatter = stateOfMatter;
+    this.behaviorsLookup = new Map(
+      this.behaviours.map((b) => [
+        b.constructor as BehaviorConstructor<Behaviour>,
+        b,
+      ])
+    );
   }
 
   update(grid: Grid, params: Params) {
     this.behaviours?.forEach((behaviour) =>
       behaviour.update(this, grid, params)
     );
+  }
+
+  getBehaviour<T extends Behaviour>(
+    behaviorClass: BehaviorConstructor<T>
+  ): T | undefined {
+    return this.behaviorsLookup.get(behaviorClass) as T | undefined;
   }
 }
 
