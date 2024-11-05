6dd49e5376abe700e78dc39950be5ec22fb839a8
Kabir <kabirsync1@gmail.com>
2024-11-05 11:09:29 +0000

    feature: add movesvertical to fire

--- src/components/simulation/materials/Fire.ts
+++ src/components/simulation/materials/Fire.ts
@@ -1,4 +1,5 @@
 import { Flammable } from "@/components/simulation/behaviours/Flammable";
+import { MovesVerticalWater } from "@/components/simulation/behaviours/MovesVerticalWater";
 import Particle from "@/components/simulation/materials/Particle";
 import { fireColor } from "@/lib/colors";
 import { Color } from "three";
@@ -13,6 +14,11 @@ export class Fire extends Particle {
       behaviours: [
         new Flammable({
           burning: true,
+        }),
+        new MovesVerticalWater({
+          maxSpeed: 0.4,
+          acceleration: -0.5,
+          initialVelocity: -0.1,
         }),
       ],
     });
