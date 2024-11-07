9b487bd0488c77b6d12a1fdbd7600cd28642ebb0
Kabir <kabirsync1@gmail.com>
2024-11-07 11:24:53 +0000

    feature: add scrollarea to options

--- src/components/simulation/EngineOptions.tsx
+++ src/components/simulation/EngineOptions.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/simulation/simulationState";
 import ThemeToggleButton from "@/components/theme/ThemeToggleButton";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useAtom } from "jotai";
 import { Pause, Play } from "lucide-react";
 
@@ -22,7 +23,7 @@ const EngineOptions = () => {
   };
 
   return (
-    <div className="flex md:flex-col min-h-36 md:w-72 border-t md:border-t-0 md:border-l border-zinc-400 dark:border-zinc-800">
+    <div className="flex md:flex-col max-h-[30%] min-h-36 md:w-72 border-t md:border-t-0 md:border-l border-zinc-400 dark:border-zinc-800">
       <div className="flex-0 border-r md:border-r-0 border-b border-zinc-400 dark:border-zinc-800 text-xs px-4 py-1">
         <div className="flex flex-col md:flex-row items-center">
           <div className="h-10 min-w-12 md:flex-1 justify-center md:justify-start flex items-center">
@@ -46,13 +47,25 @@ const EngineOptions = () => {
           </div>
         </div>
       </div>
-      <div className="flex-1 1 grid grid-cols-1 sm:grid-cols-2 content-start gap-4 p-3  border-r md:border-r-0 md:border-b border-zinc-400 dark:border-zinc-800">
-        {materialOptions.map((material) => {
-          return <MaterialButton key={material} material={material} />;
-        })}
+      <div className="md:flex-1 md:h-1/2 border-r md:border-r-0 md:border-b border-zinc-400 dark:border-zinc-800">
+        <ScrollArea className="h-[100%] w-full p-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 content-start gap-4">
+            {materialOptions.map((material) => {
+              return <MaterialButton key={material} material={material} />;
+            })}
+            {materialOptions.map((material) => {
+              return <MaterialButton key={material} material={material} />;
+            })}
+            {materialOptions.map((material) => {
+              return <MaterialButton key={material} material={material} />;
+            })}{" "}
+          </div>
+        </ScrollArea>
       </div>
-      <div className="flex-1  p-4">
-        <MaterialOptions />
+      <div className="md:flex-1 md:h-1/2">
+        <ScrollArea className="h-[100%] w-full p-4">
+          <MaterialOptions />
+        </ScrollArea>
       </div>
     </div>
   );
