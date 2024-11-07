948d1b70f70d5d11ff8ff3338c7999d8ced74969
Kabir <kabirsync1@gmail.com>
2024-11-07 15:04:05 +0000

    refactor: mobile options

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import EngineOptions from "@/components/simulation/EngineOptions";
+import MaterialButton from "@/components/simulation/MaterialButton";
+import MaterialOptions from "@/components/simulation/MaterialOptions";
+import { materialOptions } from "@/components/simulation/materials/Material";
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { LoaderIcon } from "lucide-react";
@@ -14,9 +18,9 @@ function App() {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Analytics />
       <SpeedInsights />
-      <div className="h-dvh flex flex-col dark:bg-zinc-950 bg-white text-zinc-900 dark:text-zinc-300">
+      <div className="h-dvh flex flex-col dark:bg-zinc-950 bg-white text-zinc-900 dark:text-zinc-300 ">
         <div className="h-full flex flex-col md:flex-row">
-          <div className="flex-grow bg-[#DFDFDF] dark:bg-[#010101]">
+          <div className="flex-1 bg-[#DFDFDF] dark:bg-[#010101]">
             <Suspense
               fallback={
                 <div className="w-full h-full grid place-items-center">
@@ -27,7 +31,37 @@ function App() {
               <Simulation />
             </Suspense>
           </div>
-          <EngineOptions />
+          <div className="sm:min-h-[300px] h-[40%] sm:h-[30%] flex flex-col">
+            <div className="h-[60px] border-b border-zinc-400 dark:border-zinc-800 px-3 py-2">
+              <EngineOptions />
+            </div>
+            <div className="h-[calc(100%-60px)] flex w-full">
+              <ScrollArea className="h-[100%] flex-1 ">
+                <div className="p-3">
+                  <MaterialOptions />
+                </div>
+              </ScrollArea>
+              <ScrollArea className="h-[100%] flex-0">
+                <div className="grid grid-cols-1 sm:grid-cols-2 content-start gap-4 p-3 border-l border-zinc-400 dark:border-zinc-800">
+                  {materialOptions.map((material) => {
+                    return (
+                      <MaterialButton key={material} material={material} />
+                    );
+                  })}
+                  {materialOptions.map((material) => {
+                    return (
+                      <MaterialButton key={material} material={material} />
+                    );
+                  })}
+                  {materialOptions.map((material) => {
+                    return (
+                      <MaterialButton key={material} material={material} />
+                    );
+                  })}
+                </div>
+              </ScrollArea>
+            </div>
+          </div>
         </div>
       </div>
     </ThemeProvider>
--- src/components/simulation/EngineOptions.tsx
+++ src/components/simulation/EngineOptions.tsx
@@ -1,15 +1,11 @@
-import { materialOptions } from "@/components/simulation/materials/Material";
-import MaterialButton from "@/components/simulation/materials/MaterialButton";
-import MaterialOptions from "@/components/simulation/materials/MaterialOptions";
 import SimulationOptionsButton from "@/components/simulation/SimulationOptionsButton";
 import {
   FPSAtom,
-  isPlayingAtom,
   particleSizeAtom,
+  isPlayingAtom,
 } from "@/components/simulation/simulationState";
 import ThemeToggleButton from "@/components/theme/ThemeToggleButton";
 import { Button } from "@/components/ui/button";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { useAtom } from "jotai";
 import { Pause, Play } from "lucide-react";
 
@@ -23,49 +19,25 @@ const EngineOptions = () => {
   };
 
   return (
-    <div className="flex md:flex-col max-h-[30%] min-h-36 md:w-72 border-t md:border-t-0 md:border-l border-zinc-400 dark:border-zinc-800">
-      <div className="flex-0 border-r md:border-r-0 border-b border-zinc-400 dark:border-zinc-800 text-xs px-4 py-1">
-        <div className="flex flex-col md:flex-row items-center">
-          <div className="h-10 min-w-12 md:flex-1 justify-center md:justify-start flex items-center">
-            <span>{FPS} FPS</span>
-          </div>
-          <div className="flex-1 flex flex-col md:flex-row justify-center">
-            <Button variant="ghost" size="icon" onClick={toggleIsPlaying}>
-              {isPlaying ? (
-                <Pause className="h-5 w-5" />
-              ) : (
-                <Play className="h-5 w-5" />
-              )}
-            </Button>
-            <SimulationOptionsButton
-              particleSize={particleSize}
-              setParticleSize={setParticleSize}
-            />
-          </div>
-          <div className="flex-1 flex justify-end">
-            <ThemeToggleButton />
-          </div>
-        </div>
+    <div className="flex flex-row items-center text-xs">
+      <div className="h-10 min-w-12 md:flex-1 justify-start md:justify-start flex items-center">
+        <span>{FPS} FPS</span>
       </div>
-      <div className="md:flex-1 md:h-1/2 border-r md:border-r-0 md:border-b border-zinc-400 dark:border-zinc-800">
-        <ScrollArea className="h-[100%] w-full p-4">
-          <div className="grid grid-cols-1 sm:grid-cols-2 content-start gap-4">
-            {materialOptions.map((material) => {
-              return <MaterialButton key={material} material={material} />;
-            })}
-            {materialOptions.map((material) => {
-              return <MaterialButton key={material} material={material} />;
-            })}
-            {materialOptions.map((material) => {
-              return <MaterialButton key={material} material={material} />;
-            })}{" "}
-          </div>
-        </ScrollArea>
+      <div className="flex-1 flex justify-center">
+        <Button variant="ghost" size="icon" onClick={toggleIsPlaying}>
+          {isPlaying ? (
+            <Pause className="h-5 w-5" />
+          ) : (
+            <Play className="h-5 w-5" />
+          )}
+        </Button>
+        <SimulationOptionsButton
+          particleSize={particleSize}
+          setParticleSize={setParticleSize}
+        />
       </div>
-      <div className="md:flex-1 md:h-1/2">
-        <ScrollArea className="h-[100%] w-full p-4">
-          <MaterialOptions />
-        </ScrollArea>
+      <div className="flex-1 flex justify-end">
+        <ThemeToggleButton />
       </div>
     </div>
   );
