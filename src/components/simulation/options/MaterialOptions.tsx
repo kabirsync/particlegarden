ad75bbff6cb0e4e236ed547029cf504df31a7f29
Kabir <kabirsync1@gmail.com>
2024-11-10 12:27:14 +0000

    feature: add separator and icons

--- src/components/simulation/options/MaterialOptions.tsx
+++ src/components/simulation/options/MaterialOptions.tsx
@@ -3,27 +3,46 @@ import MoveVerticalOptions from "@/components/simulation/options/MoveVerticalOpt
 import StrokeColorOptions from "@/components/simulation/options/StrokeColorOptions";
 import StrokeSizeOptions from "@/components/simulation/options/StrokeSizeOptions";
 import { selectedMaterialAtom } from "@/components/simulation/simulationState";
+import { Separator } from "@/components/ui/separator";
 import { useAtom } from "jotai";
+import { ArrowUpDown, Brush, Droplet } from "lucide-react";
 
 const MaterialOptions = () => {
   const [selectedMaterial] = useAtom(selectedMaterialAtom);
 
   return (
     <div className="flex flex-col gap-3">
-      <StrokeSizeOptions />
-      {[
-        "Sand",
-        "Wood",
-        "Water",
-        "Smoke",
-        "Acid",
-        "Gas",
-        "Oil",
-        "Stone",
-      ].includes(selectedMaterial) && <StrokeColorOptions />}
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-2 items-center">
+          <Brush className="h-4 w-4 text-zinc-300 " />
+          <span className="text-xs font-bold">Stroke</span>
+        </div>
+        <StrokeSizeOptions />
+
+        {[
+          "Sand",
+          "Wood",
+          "Water",
+          "Smoke",
+          "Acid",
+          "Gas",
+          "Oil",
+          "Stone",
+        ].includes(selectedMaterial) && <StrokeColorOptions />}
+      </div>
+      <Separator className="my-2" />
+      <div className="flex gap-2 items-center">
+        <ArrowUpDown className="h-4 w-4 text-zinc-300 " />
+        <span className="text-xs font-bold">Vertical Movement</span>
+      </div>
       {["Sand", "Water", "Smoke", "Acid", "Lava", "Gas", "Oil"].includes(
         selectedMaterial
       ) && <MoveVerticalOptions />}
+      <Separator className="my-2" />
+      <div className="flex gap-2 items-center">
+        <Droplet className="h-4 w-4 text-zinc-300 " />
+        <span className="text-xs font-bold">Liquid Movement</span>
+      </div>
       {["Water", "Smoke", "Acid", "Lava", "Oil", "Gas"].includes(
         selectedMaterial
       ) && <MoveVerticalLiquidOptions />}
--- src/components/simulation/options/StrokeSizeOptions.tsx
+++ src/components/simulation/options/StrokeSizeOptions.tsx
@@ -18,7 +18,8 @@ const StrokeSizeOptions = () => {
     <div className="flex flex-col gap-2">
       <Label htmlFor="strokeSize" className="text-xs">
         <div className="flex items-center justify-between gap-3">
-          <span>Stroke Size: </span>
+          <span className="text-zinc-400">Stroke Size: </span>
+
           <Input
             className="text-xs h-8 w-min min-w-24 mt-1"
             type="number"
