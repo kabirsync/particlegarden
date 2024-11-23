3164183cff2e3a306f3e016ef62071655020293e
Kabir <kabirsync1@gmail.com>
2024-11-23 22:51:03 +0000

    refactor: remove unused code

--- src/components/simulation/options/SimulationOptionsButton.tsx
+++ src/components/simulation/options/SimulationOptionsButton.tsx
@@ -60,11 +60,6 @@ const SimulationOptionsButton = ({
               <span className="text-xs">Dark Mode</span>{" "}
             </>
           )}
-          {/* <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="text-xs">
-            {theme === "dark" ? "Light Mode" : "Dark Mode"}
-          </span> */}
         </Button>
         <Button
           variant="ghost"
