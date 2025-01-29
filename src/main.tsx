c61ba19447f4d86c758501c80ba3b5c2665f1847
Kabir <kabirsync1@gmail.com>
2025-01-29 23:11:29 +0000

    fix: react scan bad http request

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,7 @@
-import { scan } from "react-scan";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "@/App";
-
-if (typeof window !== "undefined" && process.env.NODE_ENV !== "production") {
-  scan({
-    enabled: true,
-    log: true, // logs render info to console (default: false)
-  });
-}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
